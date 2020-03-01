130f3b3db6ab1c06af40e1aaa374bb0e5cb725e5
Federico Viceconti <Federico.Viceconti@nttdata.com>
2020-03-01 13:40:10 +0100

    adding export to arch_progress

--- lib/arch_progress.dart
+++ lib/arch_progress.dart
@@ -1,36 +1,4 @@
 library arch_progress;
 
-import 'package:arch_progress/painter/CirclePainter.dart';
-import 'package:flutter/material.dart';
-
-class ArchProgressIndicatorWidget extends StatelessWidget {
-  final Widget child;
-  final double height;
-  final double width;
-  final double currentProgress;
-
-  const ArchProgressIndicatorWidget({
-    Key key,
-    this.child,
-    this.height,
-    this.width,
-    this.currentProgress
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: CustomPaint(
-        foregroundPainter: CirclePainter(
-          currentProgress: this.currentProgress
-        ),
-        child: Container(
-          height: height,
-          width: width,
-          child: child,
-        ),
-      ),
-    );
-  }
-}
+export 'package:arch_progress/widget/arch_progress_widget.dart';
 
---
+++ lib/widget/arch_progress_widget.dart
@@ -0,0 +1,33 @@
+import 'package:arch_progress/painter/CirclePainter.dart';
+import 'package:flutter/material.dart';
+
+class ArchProgressIndicatorWidget extends StatelessWidget {
+  final Widget child;
+  final double height;
+  final double width;
+  final double currentProgress;
+
+  const ArchProgressIndicatorWidget({
+    Key key,
+    this.child,
+    this.height,
+    this.width,
+    this.currentProgress
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CustomPaint(
+        foregroundPainter: CirclePainter(
+            currentProgress: this.currentProgress
+        ),
+        child: Container(
+          height: height,
+          width: width,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
